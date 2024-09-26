4e1a7e43e4fd2851d01a6e54c0cac8ea1f38f033
Dilemma98 <emmaah98@outlook.com>
2024-09-26 12:29:09 +0200

    added github repos through api

--- script.js
+++ script.js
@@ -1,7 +1,24 @@
-setInterval(() => {
-    let currentTime = new Date();
+const reposHolder = document.getElementById("githubRepos");
 
-    hours.innerHTML = (currentTime.getHours()<10?"0":"") + currentTime.getHours();
-    minutes.innerHTML = (currentTime.getMinutes()<10?"0":"") + currentTime.getMinutes();
-    seconds.innerHTML = (currentTime.getSeconds()<10?"0":"") + currentTime.getSeconds();
+setInterval(() => {
+  let currentTime = new Date();
+
+  hours.innerHTML =
+    (currentTime.getHours() < 10 ? "0" : "") + currentTime.getHours();
+  minutes.innerHTML =
+    (currentTime.getMinutes() < 10 ? "0" : "") + currentTime.getMinutes();
+  seconds.innerHTML =
+    (currentTime.getSeconds() < 10 ? "0" : "") + currentTime.getSeconds();
 }, 1000);
+
+fetch("https://api.github.com/users/dilemma98/repos")
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (githubRepos) {
+    githubRepos.forEach((repo) => {
+      let repoItem = document.createElement("li");
+      repoItem.innerHTML = `<a href="${repo.html_url}">${repo.name}</a>`;
+      reposHolder.appendChild(repoItem);
+    });
+  });
