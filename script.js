44c2dcaf3f7818f8a204778b05fe39a538102d31
Dilemma98 <emmaah98@outlook.com>
2024-10-03 10:20:47 +0200

    Tog bort onödigt script

--- script.js
+++ script.js
@@ -1,16 +1,5 @@
 const reposHolder = document.getElementById("githubRepos");
 
-setInterval(() => {
-  let currentTime = new Date();
-
-  hours.innerHTML =
-    (currentTime.getHours() < 10 ? "0" : "") + currentTime.getHours();
-  minutes.innerHTML =
-    (currentTime.getMinutes() < 10 ? "0" : "") + currentTime.getMinutes();
-  seconds.innerHTML =
-    (currentTime.getSeconds() < 10 ? "0" : "") + currentTime.getSeconds();
-}, 1000);
-
 fetch("https://api.github.com/users/dilemma98/repos")
   .then(function (response) {
     return response.json();
